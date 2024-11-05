a4c0e2d3cf5f652dc6a5f223e5d27a8a39a1f1a3
Philipp <pcschreiber1@gmail.com>
2024-11-05 16:49:45 +0100

    Draft delete of buildings.

--- pages/views/building.py
+++ pages/views/building.py
@@ -9,64 +9,67 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class StructElement:
+    id: str
     name: str
     country: str
     material: str
     quantity: str
     emissions: str
 
+
 @dataclass
 class Building:
     name: str
     country: str
     components: list[StructElement]
-    
 
-items = Building(
-        name = "Building 1",
-        country= "India",
-        components = [StructElement(
-            name = "Component 1",
-            country = "Germany",
-            material = "wood",
-            quantity= "20",
-            emissions = "30"
+
+item = Building(
+    name="Building 1",
+    country="India",
+    components=[
+        StructElement(
+            id="1",
+            name="Component 1",
+            country="Germany",
+            material="wood",
+            quantity="20",
+            emissions="30",
         ),
-         StructElement(
-            name = "Component 2",
-            country = "Germany",
-            material = "clay",
-            quantity= "10",
-            emissions = "15"
-        )
-        ]
+        StructElement(
+            id="2",
+            name="Component 2",
+            country="Germany",
+            material="clay",
+            quantity="10",
+            emissions="15",
+        ),
+    ],
 )
 
 
 @require_http_methods(["GET", "POST", "DELETE"])
 def building(request):
-    global items
-    context = {"items": items}
-    
-    logger.info("Access list view.")
+    global item
+    context = {"items": item}
 
-    # if request.method == "POST":
-    #     new_item = request.POST.get("item")
-    #     if new_item and len(items) < 5:
-    #         logger.info("Add item: '%s' to list", new_item)
-    #         items.append(new_item)
-    #     return render(
-    #         request, "pages/home/building_list/item.html", context
-    #     )  # Partial update for POST
+    logger.info("Access list view with request: %s", request.method)
 
-    # elif request.method == "DELETE":
-    #     item_to_delete = request.GET.get("item")
-    #     if item_to_delete in items:
-    #         logger.info("Delete item '%s' from list", item_to_delete)
-    #         items.remove(item_to_delete)
-    #     return render(
-    #         request, "pages/home/building_list/item.html", context
-    #     )  # Partial update for DELETE
+    if request.method == "POST":
+        new_item = request.POST.get("item")
+        if new_item and len(item) < 5:
+            logger.info("Add item: '%s' to list", new_item)
+            item.append(new_item)
+        return render(
+            request, "pages/home/building_list/item.html", context
+        )  # Partial update for POST
+
+    elif request.method == "DELETE":
+        item_to_delete = request.GET.get("component")
+        item.components = [c for c in item.components if c.id != item_to_delete]
+        return render(
+            request, "pages/building/building_list/building_item.html", context
+        )  # Partial update for DELETE
 
     # Full page load for GET request
     logger.info("Serving full item list page for GET request")
