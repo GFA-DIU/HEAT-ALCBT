1221323fdd825f7305ed7b44badde7a0af906137
Philipp <pcschreiber1@gmail.com>
2024-12-11 18:07:10 +0100

    Create a basic interface where EPDs can be selected and de-selected

--- pages/urls.py
+++ pages/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from .views.about import AboutPageView
 from .views.home import get_building
 from .views.building import building
+from .views.assembly import component_edit, component_new
 
 urlpatterns = [
     path("", get_building, name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
-    path("building/<int:building_id>/", building, name="building")
+    path("building/<int:building_id>/", building, name="building"),
+    path("component/", component_edit, name="component"),
+    path("component/<int:assembly_id>/", component_edit, name="component_edit"),  # For editing an existing component
 ]
--- pages/views/assembly.py
+++ pages/views/assembly.py
@@ -6,8 +6,10 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.decorators.http import require_http_methods
 
-from pages.models.assembly import Assembly, AssemblyImpact
+from pages.models.assembly import Assembly
+from pages.models.epd import EPD
 from pages.forms.assembly_form import AssemblyForm
+
 
 logger = logging.getLogger(__name__)
 
@@ -17,34 +19,74 @@ def component_edit(request, assembly_id=None):
     """
     View to either edit an existing component or create a new one.
     """
-    context = {"assembly_id": assembly_id}
+    # Initialize the session variable if it doesn't exist
+    if "selected_epds" not in request.session:
+        request.session["selected_epds"] = []
 
+    # Load selected EPDs from session
+    selected_epd_ids = request.session["selected_epds"]
+    selected_epds = EPD.objects.filter(id__in=selected_epd_ids)
+
+    context = {
+        "assembly_id": assembly_id,
+        "selected_epds": selected_epds,
+    }
+    
     if assembly_id:
-        # If a component ID is provided, try to fetch the existing component
         component = get_object_or_404(Assembly, id=assembly_id)
         context["assembly_id"] = component.id
-        print(context)
-        print(request)
+
         if request.method == "POST":
-            print("Here am I")
-            form = AssemblyForm(request.POST, instance=component)
-            if form.is_valid():
-                print("Form is valid: ", request.POST)
-                form.save()
-                # return HttpResponseRedirect(reverse("component"))  # Redirect after successful update
+            if request.POST.get("action") == "form_submission":
+                print("This is a form submission")
+                form = AssemblyForm(request.POST, instance=component)
+                if form.is_valid():
+                    form.save()
+                    # Clear the session variable after successful submission
+                    request.session["selected_epds"] = []
+                    form = AssemblyForm(instance=component)
+                    epd_list = EPD.objects.all()
+                    context["epd_list"] = epd_list
+                    context["form"] = form
+                    print("This is context", context)
+                    return render(request, "pages/building/test_component.html", context)
+
+            elif request.POST.get("epd_selected"):
+                epd_id = request.POST.get("epd_selected")
+                if epd_id not in selected_epd_ids:
+                    selected_epd_ids.append(epd_id)
+                    request.session["selected_epds"] = selected_epd_ids
+            context["selected_epds"] = EPD.objects.filter(id__in=selected_epd_ids)
+            return render(request, "pages/building/component_add/selected_epd_list.html", context)
+
+        elif request.method == "DELETE":
+            # epd_id = request.body.decode("utf-8")  # Get epd_id from the request body
+            epd_id = request.GET.get("epd_remove")
+            print("This is delete: ", epd_id)
+            print(selected_epd_ids)
+            if epd_id in selected_epd_ids:
+                selected_epd_ids.remove(epd_id)
+                request.session["selected_epds"] = selected_epd_ids
+            context["selected_epds"] = EPD.objects.filter(id__in=selected_epd_ids)
+            return render(request, "pages/building/component_add/selected_epd_list.html", context)
+
         else:
-            form = AssemblyForm(
-                instance=component
-            )  # Prepopulate the form with the component data
+            form = AssemblyForm(instance=component)
+            epd_list = EPD.objects.all()
+            context["epd_list"] = epd_list
     else:
+        # Handle creation of a new assembly
         if request.method == "POST":
             form = AssemblyForm(request.POST)
             if form.is_valid():
                 form.save()
-                # return HttpResponseRedirect(reverse("component"))  # Redirect after successful creation
+                # Clear the session variable after successful submission
+                request.session["selected_epds"] = []
+                return HttpResponseRedirect(reverse("component"))
         else:
-            form = AssemblyForm()  # Blank form for new component creation
-
+            form = AssemblyForm()
+            epd_list = EPD.objects.all()
+            context["epd_list"] = epd_list
 
     context["form"] = form
     return render(request, "pages/building/test_component.html", context)
