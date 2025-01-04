7af7f15d1e657c6a02d69d5f0fa4aed9fefc5195
Philipp <pcschreiber1@gmail.com>
2025-01-04 20:31:31 +0100

    Flexibilise assembly view.

--- pages/views/assembly/assembly.py
+++ pages/views/assembly/assembly.py
@@ -21,37 +21,12 @@ def component_edit(request, building_id, assembly_id=None):
     """
     View to either edit an existing component or create a new one with pagination for EPDs.
     """
-    component = None
-    if assembly_id:
-        building_assembly = get_object_or_404(
-            BuildingAssembly.objects.select_related(),
-            assembly_id=assembly_id,
-            building_id=building_id,
-        )
-        # If there's non it will already throw an error so no need for checking
-        component = building_assembly.assembly
-    context = {
-        "assembly_id": assembly_id,
-        "building_id": building_id,
-        "epd_list": get_epd_list(request, component),
-        "epd_filters_form": EPDsFilterForm(request.POST),
-        "dimension": request.POST.get("dimension"),
-    }
-    building_instance = get_object_or_404(Building, pk=building_id)
+    assembly, building, context = set_up_view(request, building_id, assembly_id)
 
     if request.method == "POST" and request.POST.get("action") == "form_submission":
-        if not component:
-            component = Assembly()
-        save_assembly(request, component, building_instance)
-        # The redirect shortcut is not working properly with HTMX
-        # return redirect("building", building_id=building_instance.id)
-        # instead use the following:
-        response = JsonResponse({"message": "Redirecting"})
-        response["HX-Redirect"] = reverse(
-            "building", kwargs={"building_id": building_id}
-        )
-        return response
+        return handle_assembly_submission(request, assembly, building)
 
+    # Update
     elif (
         request.method == "GET"
         and request.GET.get("page")
@@ -59,7 +34,9 @@ def component_edit(request, building_id, assembly_id=None):
         and request.POST.get("action") == "filter"
     ):
         # Handle partial rendering for HTMX
+        # TODO: consider moving page logic and filtering out of "set-up"
         return render(request, "pages/assembly/epd_list.html", context)
+
 
     elif request.method == "GET" and request.GET.get("add_component") == "step_1":
         # TODO: Only makes sense for new component
@@ -68,14 +45,59 @@ def component_edit(request, building_id, assembly_id=None):
         )
 
     else:
-        if component:
-            products = Product.objects.filter(assembly=component).select_related("epd")
-            selected_epds = [SelectedEPD.parse_product(p) for p in products]
-            context["selected_epds"] = selected_epds
-        context["form"] = AssemblyForm(instance=component, building_id=building_id)
-        context["dimension"] = (
-            component.dimension if component else AssemblyDimension.AREA
-        )
+        context = handle_assembly_load(building_id, assembly, context)
 
     # Render full template for non-HTMX requests
     return render(request, "pages/assembly/editor_own_page.html", context)
+
+
+def set_up_view(request, building_id, assembly_id):
+    """Fetch objects and create baseline context."""
+    if assembly_id:
+        building_assembly = get_object_or_404(
+            BuildingAssembly.objects.select_related(),
+            assembly_id=assembly_id,
+            building_id=building_id,
+        )
+        building = building_assembly.building
+        assembly = building_assembly.assembly
+    else:
+        building = get_object_or_404(Building, pk=building_id)
+        assembly = None
+
+    context = {
+        "assembly_id": assembly_id,
+        "building_id": building_id,
+        "epd_list": get_epd_list(request, assembly),
+        "epd_filters_form": EPDsFilterForm(request.POST),
+        "dimension": request.POST.get("dimension"),  # TODO: do I need this here already?
+    }
+    return assembly, building, context
+
+
+def handle_assembly_submission(request, assembly, building):
+    if not assembly:
+        assembly = Assembly()
+    save_assembly(request, assembly, building)
+        # The redirect shortcut is not working properly with HTMX
+        # return redirect("building", building_id=building_instance.id)
+        # instead use the following:
+    response = JsonResponse({"message": "Redirecting"})
+    response["HX-Redirect"] = reverse(
+            "building", kwargs={"building_id": building.pk}
+        )
+    
+    return response
+
+
+def handle_assembly_load(building_id, assembly, context):
+    if assembly:
+        products = Product.objects.filter(assembly=assembly).select_related("epd")
+        selected_epds = [SelectedEPD.parse_product(p) for p in products]
+        context["selected_epds"] = selected_epds
+    context["form"] = AssemblyForm(instance=assembly, building_id=building_id)
+    context["dimension"] = (
+            assembly.dimension if assembly else AssemblyDimension.AREA
+        )
+    
+    return context
