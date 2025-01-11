c38f47789aebd33d8ccf4f538578067700892758
Philipp <pcschreiber1@gmail.com>
2025-01-11 16:12:40 +0100

    Add uuid to users.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.db import models
@@ -7,6 +9,7 @@ from django.dispatch import receiver
 from cities_light.models import Country, City
 
 class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True, blank=False, null=False)
 
     def __str__(self):
