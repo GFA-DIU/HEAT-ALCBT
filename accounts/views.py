1afa090ed250e2513c1d0dfb87e0f0c585326de6
rza <ramon@gfa-group.de>
2025-01-15 10:17:21 +0100

    feat(user): User can delete its account

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ import logging
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 
 from .forms import CustomUserUpdateForm, UserProfileUpdateForm
 
@@ -12,16 +13,21 @@ logger = logging.getLogger(__name__)
 @login_required
 def update_profile(request):
     if request.method == "POST":
-        user_form = CustomUserUpdateForm(request.POST, instance=request.user)
-        profile_form = UserProfileUpdateForm(
-            request.POST, instance=request.user.userprofile
-        )
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-            logger.info("Successfully updated user %s", request.user)
-            messages.success(request, "Your profile is updated successfully")
-            return redirect(to="update_profile")
+        if request.POST.get('_method') == "UPDATE":
+            user_form = CustomUserUpdateForm(request.POST, instance=request.user)
+            profile_form = UserProfileUpdateForm(
+                request.POST, instance=request.user.userprofile
+            )
+            if user_form.is_valid() and profile_form.is_valid():
+                user_form.save()
+                profile_form.save()
+                logger.info("Successfully updated user %s", request.user)
+                messages.success(request, "Your profile is updated successfully")
+                return redirect(to="update_profile")
+        elif request.POST.get('_method') == "DELETE":
+            User = get_user_model()
+            User.objects.filter(id=request.user.id).delete()
+            return redirect(to="account_login")
     else:
         user_form = CustomUserUpdateForm(instance=request.user)
         profile_form = UserProfileUpdateForm(instance=request.user.userprofile)
