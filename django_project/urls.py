b51d0f16c22ed06ba950b8809921f71bcd7a75c8
Philipp <pcschreiber1@gmail.com>
2025-01-17 12:05:43 +0100

    Scramble admin path.

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,10 +1,14 @@
+import os
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
+DJANGO_ADMIN_URL = os.environ.get("DJANGO_ADMIN_URL")
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('accounts/', include('accounts.urls')),  # include custom views
+    path(f"{DJANGO_ADMIN_URL}/admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),  # include custom views
     path("accounts/", include("allauth.urls")),
     path("", include("pages.urls")),
 ]
