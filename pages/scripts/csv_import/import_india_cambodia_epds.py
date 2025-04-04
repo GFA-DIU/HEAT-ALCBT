86e1ebfb774c007baa267783072daf58a598c9a0
djcotto <bergkoenig@posteo.de>
2025-04-04 19:27:08 +0200

    feat: load cambodia and india epds

---
+++ pages/scripts/csv_import/import_india_cambodia_epds.py
@@ -0,0 +1,95 @@
+import pandas as pd
+
+from pages.models.epd import EPD, EPDType, MaterialCategory
+from pages.scripts.csv_import.utils import (
+    add_impacts,
+    get_conversions,
+    get_country,
+    get_superuser,
+    get_category
+)
+
+import logging
+# Configure the logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)  # or the appropriate level for your use case
+
+# Create a console handler with a specific log level
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+# Create a formatter and add it to the handler
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+
+# Add the handler to the logger
+logger.addHandler(console_handler)
+
+
+impact_columns = [
+    "penrt_a1a3 [MJ]",
+    "penrt_c3 [MJ]",
+    "penrt_c4 [MJ]",
+    "penrt_d [MJ]",
+    "gwp_a1a3 [kgCo2e]",
+    "gwp_c3 [kgCo2e]",
+    "gwp_c4 [kgCo2e]",
+    "gwp_d [kgCo2e]",
+]
+
+def import_epds():
+    file_path = "pages/data/india_cambodia_EPDs_20250324.csv"
+    superuser = get_superuser()
+
+    try:
+        df = pd.read_csv(file_path, sep=";")
+    except Exception as e:
+        print(f"Error reading CSV file: {e}")
+        return
+
+    # Iterate through each row and access the data
+    success = 0
+    failure = 0
+    failure_list = []
+    for index, row in df.iterrows():
+        print(f"Row {index} is being processed.")
+        try:
+            conversions = get_conversions(row)
+            declared_amount = row.get("declared_amount", 1.0)
+            if pd.isna(declared_amount):
+                declared_amount = 1.0
+            
+            new_epd = EPD(
+                country=get_country(row["country"]),
+                source=row["source"],
+                name=row["name"],
+                names=[{"lang": "en", "value": row["name"]}],
+                public=True,
+                conversions=conversions,
+                category=get_category(row),
+                declared_unit=row["declared_unit"],
+                type=EPDType.OFFICIAL,
+                declared_amount=declared_amount,
+                comment=row['description'],
+                created_by_id=superuser.id,
+            )
+            new_epd.save()
+            add_impacts(row, new_epd, impact_columns)
+            # print(f"Row {index} processed.")
+            success += 1
+
+        except Exception as e:
+            logger.exception(f"Error in row {index}: {e}")
+            # print(f"Error in row {index}: {e}")
+            # print(f"{type(e).__name__}")
+            failure += 1
+            failure_list.append(index)
+
+    if failure == 0:
+        print(
+            f"\033[32mSuccessfully uploaded {success} generic EPDs. Errors occured in {failure} cases. Rows: {failure_list}"
+        )
+    else:
+        print(
+            f"Successfully uploaded {success} generic EPDs. Errors occured in {failure} cases. Rows: {failure_list}"
+        )
