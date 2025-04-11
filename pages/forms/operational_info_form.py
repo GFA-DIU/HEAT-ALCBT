9786777f352b57e70cf183094f283f8daa792147
Philipp <pcschreiber1@gmail.com>
2025-04-11 21:47:34 +0200

    Style the form.

--- pages/forms/operational_info_form.py
+++ pages/forms/operational_info_form.py
@@ -6,110 +6,168 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import HTML, Layout, Fieldset, Row, Column, Submit, Field
 
 
-from pages.models import Building
+from pages.models.building import Building, HeatingType, CoolingType, VentilationType, LightingType
+from pages.models.epd import Unit
 
 
 class OperationalInfoForm(forms.ModelForm):
 
+    num_residents = forms.IntegerField(
+        label="# residents",
+        help_text="Apprximation",
+        min_value=0,
+        required=False,
+        widget=forms.NumberInput(attrs={"class": "form-control", "type": "number"}),
+    )
+
+    hours_per_workday = forms.IntegerField(
+        label="Operation hours per workday",
+        help_text="hours per workday",
+        min_value=0,
+        max_value=24,
+        required=False,
+        widget=forms.NumberInput(attrs={"class": "form-control", "type": "number"}),
+    )
+
+    workdays_per_week = forms.IntegerField(
+        label="Operation days per week",
+        help_text="days per week",
+        min_value=0,
+        max_value=7,
+        required=False,
+        widget=forms.NumberInput(attrs={"class": "form-control", "type": "number"}),
+    )
+
+    weeks_per_year = forms.IntegerField(
+        label="Operation weeks per year",
+        help_text="weeks per year",
+        min_value=0,
+        max_value=52,
+        required=False,
+        widget=forms.NumberInput(attrs={"class": "form-control", "type": "number"}),
+    )
+
+    heating_temp = forms.DecimalField(
+        label="Typical Heating Temperature",
+        min_value=0,
+        max_value=120,
+        decimal_places=2,
+        required=False,
+        widget=forms.NumberInput(attrs={"class": "form-control", "type": "number"}),
+    )
+
+    heating_temp_unit = forms.ChoiceField(
+        label="Heating Temperature Unit",
+        choices=[c for c in Unit.choices if c[0] in (Unit.CELSIUS, Unit.FAHRENHEIT)],
+        required=False,
+        widget=forms.Select(attrs={"class": "form-select"}),
+    )
+
+    cooling_temp = forms.DecimalField(
+        label="Typical Cooling Temperature",
+        min_value=0,
+        max_value=120,
+        decimal_places=2,
+        required=False,
+        widget=forms.NumberInput(attrs={"class": "form-control", "type": "number"}),
+    )
+
+    cooling_temp_unit = forms.ChoiceField(
+        label="Cooling Temperature Unit",
+        choices=[c for c in Unit.choices if c[0] in (Unit.CELSIUS, Unit.FAHRENHEIT)],
+        required=False,
+        widget=forms.Select(attrs={"class": "form-select"}),
+    )
+
+    heating_type = forms.ChoiceField(
+        label="Heating Type",
+        choices=[('', '---------')] + HeatingType.choices,
+        required=False,
+        widget=forms.Select(attrs={"class": "form-select"}),
+    )
+
+    heating_capacity = forms.DecimalField(
+        label="Heating Capacity",
+        min_value=0,
+        decimal_places=2,
+        required=False,
+        widget=forms.NumberInput(attrs={"class": "form-control", "type": "number"}),
+    )
+
+    heating_unit = forms.ChoiceField(
+        label="Heating Unit",
+        choices=[c for c in Unit.choices if c[0] in (Unit.KW,)],
+        required=False,
+        initial="",
+        widget=forms.Select(attrs={"class": "form-select"}),
+    )
+
+    cooling_type = forms.ChoiceField(
+        label="Coolingting Type",
+        choices=[('', '---------')]+CoolingType.choices,
+        required=False,
+        widget=forms.Select(attrs={"class": "form-select"}),
+    )
+
+    cooling_capacity = forms.DecimalField(
+        label="Cooling Capacity",
+        min_value=0,
+        decimal_places=2,
+        required=False,
+        widget=forms.NumberInput(attrs={"class": "form-control", "type": "number"}),
+    )
+
+    cooling_unit = forms.ChoiceField(
+        label="Cooling Unit",
+        choices=[c for c in Unit.choices if c[0] in (Unit.KW, Unit.TR)],
+        required=False,
+        widget=forms.Select(attrs={"class": "form-select"}),
+    )
+
+    ventilation_type = forms.ChoiceField(
+        label="Ventilation Type",
+        choices=[('', '---------')]+VentilationType.choices,
+        required=False,
+        widget=forms.Select(attrs={"class": "form-select"}),
+    )
+
+    ventilation_capacity = forms.DecimalField(
+        label="Ventilation Capacity",
+        min_value=0,
+        decimal_places=2,
+        required=False,
+        widget=forms.NumberInput(attrs={"class": "form-control", "type": "number"}),
+    )
+
+    ventilation_unit = forms.ChoiceField(
+        label="Ventilation Unit",
+        choices=[c for c in Unit.choices if c[0] in (Unit.M3_H, Unit.CFM)],
+        required=False,
+        widget=forms.Select(attrs={"class": "form-select"}),
+    )
+
+    lighting_type = forms.ChoiceField(
+        label="Lighting Type",
+        choices=[('', '---------')]+LightingType.choices,
+        required=False,
+        widget=forms.Select(attrs={"class": "form-select"}),
+    )
+
+    lighting_capacity = forms.DecimalField(
+        label="Lighting Capacity",
+        min_value=0,
+        decimal_places=2,
+        required=False,
+        widget=forms.NumberInput(attrs={"class": "form-control", "type": "number"}),
+    )
+
+    lighting_unit = forms.ChoiceField(
+        label="Lighting Unit",
+        choices=[c for c in Unit.choices if c[0] in (Unit.KW,)],
+        required=False,
+        widget=forms.Select(attrs={"class": "form-select"}),
+    )
+
     class Meta:
         model = Building
-        fields = [
-            "num_residents",
-            "weeks_per_year",
-            "hours_per_workday",
-            "heating_temp",
-            "heating_temp_unit",
-            "workdays_per_week",
-            "cooling_temp",
-            "cooling_temp_unit",
-            "heating_type",
-            "heating_capacity",
-            "heating_unit",
-            "cooling_type",
-            "cooling_capacity",
-            "cooling_unit",
-            "ventilation_type",
-            "ventilation_capacity",
-            "ventilation_unit",
-            "lighting_type",
-            "lighting_capacity",
-            "lighting_unit",
-        ]
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["num_residents"].validators.extend([MinValueValidator(0)])
-        self.fields["hours_per_workday"].validators.extend(
-            [MinValueValidator(0), MaxValueValidator(24)]
-        )
-        self.fields["workdays_per_week"].validators.extend(
-            [MinValueValidator(0), MaxValueValidator(7)]
-        )
-        self.fields["weeks_per_year"].validators.extend(
-            [MinValueValidator(0), MaxValueValidator(52)]
-        )
-        self.fields["heating_temp"].validators.extend(
-            [MinValueValidator(0), MaxValueValidator(120)]
-        )
-        self.fields["cooling_temp"].validators.extend(
-            [MinValueValidator(0), MaxValueValidator(120)]
-        )
-        self.fields["heating_capacity"].validators.extend([MinValueValidator(0)])
-        self.fields["cooling_capacity"].validators.extend([MinValueValidator(0)])
-        self.fields["ventilation_capacity"].validators.extend([MinValueValidator(0)])
-        self.fields["lighting_capacity"].validators.extend([MinValueValidator(0)])
-        # Crispy Forms Layout
-        self.helper = FormHelper()
-        self.helper.layout = Layout(
-            # Location Information Section
-            Fieldset(
-                "Location",
-                # Name and Category in the first row
-                Row(
-                    Column("num_residents", css_class="col-md-6"),
-                    Column(Field("weeks_per_year"), css_class="col-md-6"),
-                ),
-                # Country, ZIP and City in the second row
-                Row(
-                    Column(Field("hours_per_workday"), css_class="col-md-6"),
-                    Column(Field("heating_temp"), css_class="col-md-4"),
-                    Column(Field("heating_temp_unit"), css_class="col-md-2"),
-                ),
-                # Country, ZIP and City in the second row
-                Row(
-                    Column("workdays_per_week", css_class="col-md-6"),
-                    Column(Field("cooling_temp"), css_class="col-md-4"),
-                    Column(Field("cooling_temp_unit"), css_class="col-md-2"),
-                ),
-            ),
-            HTML("<hr>"),  # Add a horizontal rule
-            Fieldset(
-                "Category",
-                # Street and Number
-                Row(
-                    Column(Field("heating_type"), css_class="col-md-6"),
-                    Column(Field("heating_capacity"), css_class="col-md-4"),
-                    Column(Field("heating_unit"), css_class="col-md-2"),
-                ),
-                # Street and Number
-                Row(
-                    Column(Field("cooling_type"), css_class="col-md-6"),
-                    Column(Field("cooling_capacity"), css_class="col-md-4"),
-                    Column(Field("cooling_unit"), css_class="col-md-2"),
-                ),
-                # Street and Number
-                Row(
-                    Column(Field("ventilation_type"), css_class="col-md-6"),
-                    Column(Field("ventilation_capacity"), css_class="col-md-4"),
-                    Column(Field("ventilation_unit"), css_class="col-md-2"),
-                ),
-                # Street and Number
-                Row(
-                    Column(Field("lighting_type"), css_class="col-md-6"),
-                    Column(Field("lighting_capacity"), css_class="col-md-4"),
-                    Column(Field("lighting_unit"), css_class="col-md-2"),
-                ),
-            ),
-            # Update building_simulation.py line 44 if this changes
-            Submit("submit", "Submit", css_class="btn btn-primary"),
-        )
+        fields = []
